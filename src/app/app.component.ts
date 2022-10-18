bceb278e6f74c4ac23c6f55f53564a82e63dd5d3
ssadrian <113125604+ssadrian@users.noreply.github.com>
2022-10-18 20:13:32 +0200

    Finish task

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,12 +18,19 @@ type GameState = "running" | "won";
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'StructuredDirectives';
+  title: string = 'StructuredDirectives';
 
-  emoteLocations: IEmoteLocation[] = [];
+  #waitTimeMillis: number = 750;
+
+  #emoteLocations: IEmoteLocation[] = [];
   emoteButtons: IClickableEmote[] = [];
   pairsFound: number = 0;
+  pairsToFind: number = 0;
   gameState: GameState = "running";
+  areButtonsClickable: boolean = true;
+  defaultButtonCover: string = "assets/emotes/party.png";
+
+  #lastSelectedEmote?: IClickableEmote;
 
   ngOnInit(): void {
     let emoteNames: string[] = [
@@ -32,11 +39,11 @@ export class AppComponent {
 
     let extension: string = "png";
     let basePath: string = "./assets/emotes";
-    emoteNames.forEach(item => {
+    emoteNames.forEach((item: string): void => {
       let emote: IEmoteLocation = {name: item, location: `${basePath}/${item}.${extension}`};
 
-      for (let i = 0; i < 2; i++) {
-        this.emoteLocations.push(emote);
+      for (let i: number = 0; i < 2 && !this.defaultButtonCover.includes(item); i++) {
+        this.#emoteLocations.push(emote);
         this.emoteButtons.push({wasClicked: false, info: emote});
       }
     });
@@ -45,13 +52,17 @@ export class AppComponent {
   }
 
   initializeGame(): void {
+    this.pairsToFind = this.emoteButtons.length / 2;
     this.pairsFound = 0;
     this.gameState = "running";
-    this.shuffle<IClickableEmote>(this.emoteButtons);
+    this.#shuffle<IClickableEmote>(this.emoteButtons);
+    this.emoteButtons.forEach((emoteBtn: IClickableEmote): void => {
+      emoteBtn.wasClicked = false;
+    });
   }
 
-  shuffle<T>(array: T[]): T[] {
-    let currentIndex = array.length, randomIndex;
+  #shuffle<T>(array: T[]): T[] {
+    let currentIndex: number = array.length, randomIndex;
 
     // While there remain elements to shuffle.
     while (currentIndex != 0) {
@@ -67,4 +78,46 @@ export class AppComponent {
 
     return array;
   };
+
+  imageClick(clickableEmote: IClickableEmote): void {
+    // Avoid emote matching on same emote button
+    if (clickableEmote.wasClicked) {
+      return;
+    }
+
+    clickableEmote.wasClicked = true;
+
+    // Remember the last clicked/selected emote
+    if (this.#lastSelectedEmote == undefined) {
+      this.#lastSelectedEmote = clickableEmote;
+      return;
+    }
+
+    // When emotes match their file/info name
+    if (this.#lastSelectedEmote.info.name == clickableEmote.info.name) {
+      this.pairsFound++;
+      this.#lastSelectedEmote = undefined;
+      this.#checkEndOfGame();
+      return;
+    }
+
+    // Emotes are not matching, disallow clicking again
+    this.areButtonsClickable = false;
+
+    setTimeout((): void => {
+      if (this.#lastSelectedEmote == null) {
+        return;
+      }
+
+      // Hide the last selected and current emote
+      this.#lastSelectedEmote.wasClicked = clickableEmote.wasClicked = false;
+      this.#lastSelectedEmote = undefined;
+
+      this.areButtonsClickable = true;
+    }, this.#waitTimeMillis);
+  }
+
+  #checkEndOfGame(): void {
+    this.gameState = this.pairsFound == this.pairsToFind ? "won" : "running";
+  }
 }
